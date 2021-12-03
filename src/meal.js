b7441e95a554251a998184ebf22a750c7467ea8b
Geena Jackson <gjacks303@gmail.com>
2021-12-02 18:50:42 -0700

    Add meal

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
-
+function nameMenuItem(menuItemName) {
+  return `Delicious ${menuItemName}`;
 }
 
+function createMenuItem(menuItemName, price, type) {
+  var menuItem = {
+    name: menuItemName,
+    price: price,
+    type: type,
+  }
+  return menuItem;
+}
+
+function addIngredients(ingredients, array) {
+  if (array.includes(ingredients)) {
+  } else {
+    return array.push(ingredients);
+  }
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`;
+}
+
+function decreasePrice(price) {
+  return price - (price * .1);
+}
+
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type,
+  }
+  return recipe
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
