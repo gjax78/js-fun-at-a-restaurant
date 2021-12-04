6fcd957eccc4395b9b2f0d6d15c30303243ffbc0
Geena Jackson <gjacks303@gmail.com>
2021-12-04 12:41:46 -0700

    Add order

--- src/order.js
+++ src/order.js
@@ -1,7 +1,38 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    return deliveryOrders.push(order);
+  }
+}
+
+function refundOrder (order, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === order) {
+      deliveryOrders.splice(i, 1);
+    }
+  }
+}
+
+function listItems(name) {
+  var orderNum = []
+  for (var i = 0; i < name.length; i++) {
+    orderNum.push(name[i].item)
+  }
+  return orderNum.join(', ');
+}
+
+function searchOrder(deliveryOrders, name) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item == name) {
+      return true;
+    }
+  }
+    return false;
+}
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
