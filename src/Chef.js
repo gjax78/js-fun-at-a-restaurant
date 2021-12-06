dab84d40448e3fb4441c2aafaae07f62c8f50f7b
Geena Jackson <gjacks303@gmail.com>
2021-12-05 21:02:48 -0700

    Add chef

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,24 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
   }
-}
+
+  greetCustomer(name, boolean) {
+    if (boolean === true) {
+      return `Good morning, ${name}!`
+    }
+    else return `Hello, ${name}!`
+  }
+  checkForFood(item, name) {
+    //function checkForFood(item) {
+      //for (var i = 0; i < item.length; i++) {
+        if (item.name === name) {
+          return `Yes, we're serving ${name} today!`;
+        }
+      }
+    }
+  //}
+//}
 
 module.exports = Chef;
